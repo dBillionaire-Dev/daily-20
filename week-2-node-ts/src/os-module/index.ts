0bccfc8df18cf747a3b2e9c2a24b1e6ec185d4eb
dBillionaire-Dev <nezerekunke@gmail.com>
2026-03-31 22:07:26 +0100

    os-module

---
+++ week-2-node-ts/src/os-module/index.ts
@@ -0,0 +1,16 @@
+/**
+ * OS MODULE IN NODE.JS
+ * The OS module provides a number of operating system-related utility methods.
+ * It can be accessed using:
+ * * import os from 'os';
+ *
+ * Some of the commonly used methods in the OS module include:
+ * * os.platform(): Returns the platform of the operating system (e.g., 'win32', 'darwin', 'linux').
+ * * os.arch(): Returns the architecture of the operating system (e.g., 'x64', 'arm').
+ * * os.cpus(): Returns an array of objects containing information about each CPU/core installed.
+ * * os.freemem(): Returns the amount of free memory in bytes.
+ * * os.totalmem(): Returns the total amount of memory in bytes.
+ * * os.uptime(): Returns the system uptime in seconds.
+ * * os.homedir(): Returns the home directory of the current user.
+ * * os.tmpdir(): Returns the default directory for temporary files.
+ */
