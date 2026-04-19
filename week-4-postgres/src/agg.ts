4124b4d7ff1a3c80ca0de618950d11761ab9d23f
dBillionaire-Dev <nezerekunke@gmail.com>
2026-04-19 18:14:38 +0100

    Aggregate functions in postgresql

---
+++ week-4-postgres/src/agg.ts
@@ -0,0 +1,8 @@
+/**
+ * AGGREGATE FUNCTIONS:
+ *
+ * * Aggregate functions perform a calculation on a set of values and return a single value.
+ * * They are often used with the GROUP BY clause to group the result set by one or more columns.
+ *
+ *
+ */
